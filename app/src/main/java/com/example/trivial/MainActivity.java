9f8db83df1a06fc96f6d109ee4501211e0839066
jorgelomen <jorge.lopmen@gmail.com>
2021-01-14 20:46:34 +0100

    Add Jugador en el MainActivity

--- app/src/main/java/com/example/trivial/MainActivity.java
+++ app/src/main/java/com/example/trivial/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.trivial;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+   private Button addjugador, start, load;
+   private ImageView avatar;
+   private EditText nombrePlayer;
+
+    int [] avatares= {R.drawable.hombre, R.drawable.mujer};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addjugador=(Button) findViewById(R.id.AddJugador);
+        start=(Button) findViewById(R.id.Start);
+        load=(Button) findViewById(R.id.Load);
+        avatar=(ImageView) findViewById(R.id.Avatar);
+        nombrePlayer=(EditText) findViewById(R.id.Njugador);
+
+        addjugador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AddPlayer();
+            }
+        });
+
+
     }
+
+    public void AddPlayer(){
+        Random r1=new Random();
+        int i1= r1.nextInt(2);
+
+        ImageView avatar1 = new ImageView(this);
+        EditText nombrePlayer = new EditText(this);
+
+        avatar.setImageResource(avatares[i1]);
+    }
+
+
+
 }
