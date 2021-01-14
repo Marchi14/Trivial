ca54c09984256bca33009ffb3b78d97b254d89a8
Marchi14 <marcos_wwe@hotmail.es>
2021-01-14 23:04:10 +0100

    Color

--- app/src/main/java/com/example/trivial/JuegoActivity.java
+++ app/src/main/java/com/example/trivial/JuegoActivity.java
@@ -4,22 +4,25 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class JuegoActivity extends AppCompatActivity {
-
-
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego);
         ImageView tablero = findViewById(R.id.tablero);
         BitmapDrawable drawable = (BitmapDrawable) tablero.getDrawable();
         Bitmap bitmap = drawable.getBitmap();
-        bitmap.getColor(150,150);
+        int pixel=bitmap.getPixel(862,45);
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(String.format("#%02X%02X%02X", Color.red(pixel), Color.green(pixel),Color.blue(pixel)));
     }
 }
