ebc6a00aafa1e169c084301934ca79e7b3a1d1f6
Marchi14 <marcos_wwe@hotmail.es>
2021-01-18 21:36:50 +0100

    Movimiento Ficha

--- app/src/main/java/com/example/trivial/JuegoActivity.java
+++ app/src/main/java/com/example/trivial/JuegoActivity.java
@@ -4,20 +4,20 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class JuegoActivity extends AppCompatActivity {
 
     Bitmap bmp;
     ImageView tablero, cursor;
-
+    private static final int angulo_casilla = 10;
+    int ndado=2;
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
@@ -26,22 +26,68 @@ public class JuegoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_juego);
         tablero = findViewById(R.id.tablero);
         cursor = findViewById(R.id.cursor);
-        Button b = findViewById(R.id.button);
+        Button bI = findViewById(R.id.bIzquierda);
+        Button bD = findViewById(R.id.bDerecha);
         bmp = ((BitmapDrawable)tablero.getDrawable()).getBitmap();
         bmp=bmp.copy(Bitmap.Config.ARGB_8888,true);
+        ImageView dado = findViewById(R.id.dado);
 
-        b.setOnClickListener(new View.OnClickListener() {
+       // dado.setOnTouchListener(new View.OnTouchListener() {
+       //     @Override
+       //     public boolean onTouch(View v, MotionEvent event) {
+       //         dadoRNG();
+       //     }
+       // });
+//
+        bI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MoverCursorIzquierda();
+                GetTypeofQuestions(bI);
+            }
+        });
+
+        bD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MoverCursorDerecha();
+                GetTypeofQuestions(bD);
             }
         });
     }
 
-    private void MoverCursorIzquierda() {
+    //private void dadoRNG() {
+//
+    //}
+
+    private void MoverCursorDerecha() {
+        int fX;
+        int fY;
+        int angulo = angulo_casilla*ndado;
+        int cX = (int) cursor.getX();
+        int cY = (int) cursor.getY();
+        double d = Math.sin(angulo);
+        fX = (int) (cX + d*Math.cos(angulo));
+        fY = (int) (cY + d*Math.sin(angulo));
+        cursor.setX(fX);
+        cursor.setY(fY);
     }
 
-    private void GetQuestions() {
+    private void MoverCursorIzquierda() {
+        int fX;
+        int fY;
+        int angulo = angulo_casilla*ndado;
+        int cX = (int) cursor.getX();
+        int cY = (int) cursor.getY();
+        double d = Math.sin(angulo) * 10;
+        fX = (int) (cX - d*Math.cos(angulo));
+        fY = (int) (cY + d*Math.sin(angulo));
+        cursor.setX(fX);
+        cursor.setY(fY);
+    }
+
+    private void GetTypeofQuestions(Button b) {
         int pixel=bmp.getPixel((int) (cursor.getX() + (cursor.getWidth()/2)),(int) cursor.getY() + (cursor.getHeight()/2));
+        b.setBackgroundColor(pixel);
     }
 }
