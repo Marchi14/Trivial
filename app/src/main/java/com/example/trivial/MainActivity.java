7263bf17bf4c7bf8f8ec8c2d64bfd8f2e2c1e13a
Marchi14 <marcos_wwe@hotmail.es>
2021-01-19 20:17:56 +0100

    Movimiento Ficha

--- app/src/main/java/com/example/trivial/MainActivity.java
+++ app/src/main/java/com/example/trivial/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.trivial;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -40,7 +41,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IniciarPartida();
+            }
+        });
 
+
+    }
+
+    private void IniciarPartida() {
+        Intent intent = new Intent(this,JuegoActivity.class);
+        startActivity(intent);
     }
 
     public void AddPlayer(){
@@ -59,7 +72,4 @@ public class MainActivity extends AppCompatActivity {
         contenedor.addView(avatar1,44, 45);
         contenedor.addView(nombrePlayer,140,38);
     }
-
-
-
 }
