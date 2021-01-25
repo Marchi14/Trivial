7c1ec0968ccb7a4ec99eeb1b1060d5d1045e0101
admin <marcos_wwe@hotmail.es>
2021-01-25 13:57:55 +0100

    Jugador

--- app/src/main/java/com/example/trivial/Jugador.java
+++ app/src/main/java/com/example/trivial/Jugador.java
@@ -1,4 +1,12 @@
 package com.example.trivial;
 
 public class Jugador {
+    String Nombre;
+    int casillaX;
+    int casillaY;
+    boolean Quesito_amarillo, Quesito_rosa, Quesito_azul,Quesito_morado,Quesito_naranja,Quesito_verde;
+
+    public Jugador(String Nombre,int casillaX, int casillaY, boolean Quesito_amarillo, boolean Quesito_rosa, boolean Quesito_azul, boolean Quesito_morado, boolean Quesito_naranja, boolean Quesito_verde){
+
+    }
 }
