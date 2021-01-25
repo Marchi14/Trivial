fcb7f8f25d023dc606bd02e5b8013d302c9ebf23
admin <marcos_wwe@hotmail.es>
2021-01-25 12:44:26 +0100

    Movimiento Casi final (faltan casillas grandes y rectas)

--- app/src/main/java/com/example/trivial/JuegoActivity.java
+++ app/src/main/java/com/example/trivial/JuegoActivity.java
@@ -3,6 +3,7 @@ package com.example.trivial;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.Point;
@@ -15,15 +16,17 @@ import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 
-public class JuegoActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class JuegoActivity extends AppCompatActivity implements View.OnTouchListener {
 
     Bitmap bmp;
-    ImageView tablero, cursor;
-    Button bI,bD;
-    int cX,cY;
+    ImageView tablero, cursor, dado;
+    Button bI, bD;
+    int cX, cY;
     Point[] points = new Point[2];
-    int angulo = ((360/6)/7);
-    int ndado=1;
+    int angulo = ((360 / 6) / 8);
+    int ndado = 1;
 
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
@@ -34,22 +37,15 @@ public class JuegoActivity extends AppCompatActivity {
         cursor = findViewById(R.id.cursor);
         bI = findViewById(R.id.bIzquierda);
         bD = findViewById(R.id.bDerecha);
-        bmp = ((BitmapDrawable)tablero.getDrawable()).getBitmap();
-        bmp=bmp.copy(Bitmap.Config.ARGB_8888,true);
-        ImageView dado = findViewById(R.id.dado);
-
-        dado.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return false;
-            }
-        });
+        bmp = ((BitmapDrawable) tablero.getDrawable()).getBitmap();
+        bmp = bmp.copy(Bitmap.Config.ARGB_8888, true);
+        dado = findViewById(R.id.dado);
+        dado.setOnTouchListener(this);
 
         bI.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MoverIzquierda();
-                DetectarCasillas(bI,bD);
             }
         });
 
@@ -57,7 +53,6 @@ public class JuegoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 MoverDerecha();
-                DetectarCasillas(bI,bD);
             }
         });
     }
@@ -72,41 +67,48 @@ public class JuegoActivity extends AppCompatActivity {
         cursor.setY(points[1].y);
     }
 
-    private void DetectarCasillas(Button bI,Button bD){
+    private void DetectarCasillas(Button bI, Button bD) {
         Point Izq;
         Point Der;
-        Izq=Izquierda(cX,cY);
-        Der=Derecha(cX,cY);
-        int pixel=bmp.getPixel(Izq.x,Izq.y);
+        cX= (int) cursor.getX();
+        cY= (int) cursor.getY();
+        Izq = Izquierda(cX, cY);
+        Der = Derecha(cX, cY);
+        int pixel = bmp.getPixel(Izq.x, Izq.y);
         bI.setBackgroundColor(pixel);
-        pixel=bmp.getPixel(Der.x,Der.y);
+        pixel = bmp.getPixel(Der.x, Der.y);
         bD.setBackgroundColor(pixel);
-        points[0]=Izq;
-        points[1]=Der;
+        points[0] = Izq;
+        points[1] = Der;
     }
 
 
-    private Point Derecha(int cX,int cY) {
+    private Point Derecha(int cX, int cY) {
         int fX;
-        int fY=cY;
-        int h = bmp.getWidth()/2;
+        int fY = cY;
+        int h = bmp.getWidth() / 2;
         if (cY < h) {
-            fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) - (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
-            if (cX < h)
-                fY = (int) (((cX - h) * (-Math.sin(Math.toRadians(angulo * ndado)))) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
-            else
+            if (cX < h){
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) - (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
                 fY = (int) ((cX - h) * Math.sin(Math.toRadians(angulo * ndado)) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
-        }
-        else{
-            fX = (int) ((cX-h)*Math.cos(Math.toRadians(angulo*ndado))+(cY-h)*Math.sin(Math.toRadians(angulo*ndado)));
-            if (cX < h)
+            }
+            else{
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) - (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
                 fY = (int) ((cX - h) * Math.sin(Math.toRadians(angulo * ndado)) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
-            else
+            }
+        } else {
+            if (cX < h){
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) + (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
                 fY = (int) (((cX - h) * (-Math.sin(Math.toRadians(angulo * ndado)))) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
+            }
+            else{
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) - (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
+                fY = (int) ((cX - h) * Math.sin(Math.toRadians(angulo * ndado)) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
+            }
         }
         fX += h;
         fY += h;
-        return new Point(fX,fY);
+        return new Point(fX, fY);
     }
 
     private Point Izquierda(int cX, int cY) {
@@ -114,20 +116,56 @@ public class JuegoActivity extends AppCompatActivity {
         int fY;
         int h = bmp.getWidth() / 2;
         if (cY < h) {
-            fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) + (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
-            if (cX < h)
-                fY = (int) ((cX - h) * Math.sin(Math.toRadians(angulo * ndado)) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
-            else
+            if (cX < h){
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) + (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
                 fY = (int) (((cX - h) * (-Math.sin(Math.toRadians(angulo * ndado)))) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
+            }
+            else{
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) + (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
+                fY = (int) (((cX - h) * (-Math.sin(Math.toRadians(angulo * ndado)))) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
+            }
         } else {
-            fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) - (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
-            if (cX < h)
-                fY = (int) (((cX - h) * (-Math.sin(Math.toRadians(angulo * ndado)))) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
-            else
+            if (cX < h){
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) - (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
                 fY = (int) ((cX - h) * Math.sin(Math.toRadians(angulo * ndado)) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
+            }
+            else{
+                fX = (int) ((cX - h) * Math.cos(Math.toRadians(angulo * ndado)) + (cY - h) * Math.sin(Math.toRadians(angulo * ndado)));
+                fY = (int) (((cX - h) * (-Math.sin(Math.toRadians(angulo * ndado)))) + (cY - h) * Math.cos(Math.toRadians(angulo * ndado)));
+            }
         }
         fX += h;
         fY += h;
         return new Point(fX, fY);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            Random rng = new Random();
+            ndado = rng.nextInt(6)+1;
+            switch (ndado) {
+                case 1:
+                    dado.setImageResource(R.drawable.dado1);
+                    break;
+                case 2:
+                    dado.setImageResource(R.drawable.dado2);
+                    break;
+                case 3:
+                    dado.setImageResource(R.drawable.dado3);
+                    break;
+                case 4:
+                    dado.setImageResource(R.drawable.dado4);
+                    break;
+                case 5:
+                    dado.setImageResource(R.drawable.dado5);
+                    break;
+                case 6:
+                    dado.setImageResource(R.drawable.dado6);
+                    break;
+            }
+            DetectarCasillas(bI, bD);
+        }
+        return false;
     }
 }
