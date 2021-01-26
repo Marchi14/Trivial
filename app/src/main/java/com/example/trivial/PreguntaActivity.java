21947cb5c7cd938d602157ab6a044580c25d7fad
Marchi14 <marcos_wwe@hotmail.es>
2021-01-26 20:11:30 +0100

    Cambio de pregunta de Fragment a Activity y modificacion de tablero.png

---
+++ app/src/main/java/com/example/trivial/PreguntaActivity.java
@@ -0,0 +1,15 @@
+package com.example.trivial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreguntaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregunta);
+
+    }
+}
