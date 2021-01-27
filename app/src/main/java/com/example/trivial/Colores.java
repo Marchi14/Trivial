b50a7b06fd46f280cc5798589e3d734aa6bfa2ef
Marchi14 <marcos_wwe@hotmail.es>
2021-01-27 22:33:43 +0100

    Cambios JuegoActivity

--- app/src/main/java/com/example/trivial/Colores.java
+++ app/src/main/java/com/example/trivial/Colores.java
@@ -1,19 +1,19 @@
 package com.example.trivial;
 
 public enum Colores {
-    Naranja("F17B35"),
-    qNaranja("E4732F"),
-    Verde("4DAB53"),
-    qVerde("4D9E53"),
-    Rosa("FD85B3"),
-    qRosa("F385B3"),
-    Amarillo("EFDB3A"),
-    qAmarillo("E5D32B"),
-    Morado("9A47E0"),
-    qMorado("8E40D1"),
-    Azul("5FBFCD"),
-    qAzul("5FADCD"),
-    Blanco("EBCFC3");
+    Naranja("#F17B35"),
+    qNaranja("#E4732F"),
+    Verde("#4DAB53"),
+    qVerde("#4D9E53"),
+    Rosa("#FD85B3"),
+    qRosa("#F385B3"),
+    Amarillo("#EFDB3A"),
+    qAmarillo("#E5D32B"),
+    Morado("#9A47E0"),
+    qMorado("#8E40D1"),
+    Azul("#5FBFCD"),
+    qAzul("#5FADCD"),
+    Blanco("#EBCFC3");
 
 
     private final String color;
@@ -22,8 +22,7 @@ public enum Colores {
         this.color = color;
     }
 
-    public String getHEXColor()
-    {
-        return this.color;
+    public String getColor() {
+        return color;
     }
 }
--- app/src/main/java/com/example/trivial/Jugador.java
+++ app/src/main/java/com/example/trivial/Jugador.java
@@ -4,9 +4,29 @@ public class Jugador {
     String Nombre;
     int casillaX;
     int casillaY;
-    boolean Quesito_amarillo, Quesito_rosa, Quesito_azul,Quesito_morado,Quesito_naranja,Quesito_verde;
+    boolean Quesito_amarillo, Quesito_rosa, Quesito_azul,Quesito_morado,Quesito_naranja,Quesito_verde, turno;
 
-    public Jugador(String Nombre,int casillaX, int casillaY, boolean Quesito_amarillo, boolean Quesito_rosa, boolean Quesito_azul, boolean Quesito_morado, boolean Quesito_naranja, boolean Quesito_verde){
+    public Jugador(String Nombre){
+        this.Nombre=Nombre;
+        this.Quesito_amarillo=false;
+        this.Quesito_rosa=false;
+        this.Quesito_azul=false;
+        this.Quesito_morado=false;
+        this.Quesito_naranja=false;
+        this.Quesito_verde=false;
+        this.turno=false;
+    }
 
+    public Jugador(String nombre, int casillaX, int casillaY, boolean quesito_amarillo, boolean quesito_rosa, boolean quesito_azul, boolean quesito_morado, boolean quesito_naranja, boolean quesito_verde, boolean turno) {
+        Nombre = nombre;
+        this.casillaX = casillaX;
+        this.casillaY = casillaY;
+        Quesito_amarillo = quesito_amarillo;
+        Quesito_rosa = quesito_rosa;
+        Quesito_azul = quesito_azul;
+        Quesito_morado = quesito_morado;
+        Quesito_naranja = quesito_naranja;
+        Quesito_verde = quesito_verde;
+        this.turno = turno;
     }
 }
