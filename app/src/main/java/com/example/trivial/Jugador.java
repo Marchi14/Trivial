42aa77fef6ae63931f2a7a2313040801b6efb389
Marchi14 <marcos_wwe@hotmail.es>
2021-02-03 18:17:18 +0100

    Quesitos

--- app/src/main/java/com/example/trivial/Jugador.java
+++ app/src/main/java/com/example/trivial/Jugador.java
@@ -29,4 +29,84 @@ public class Jugador {
         Quesito_verde = quesito_verde;
         this.turno = turno;
     }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getCasillaX() {
+        return casillaX;
+    }
+
+    public void setCasillaX(int casillaX) {
+        this.casillaX = casillaX;
+    }
+
+    public int getCasillaY() {
+        return casillaY;
+    }
+
+    public void setCasillaY(int casillaY) {
+        this.casillaY = casillaY;
+    }
+
+    public boolean isQuesito_amarillo() {
+        return Quesito_amarillo;
+    }
+
+    public void setQuesito_amarillo(boolean quesito_amarillo) {
+        Quesito_amarillo = quesito_amarillo;
+    }
+
+    public boolean isQuesito_rosa() {
+        return Quesito_rosa;
+    }
+
+    public void setQuesito_rosa(boolean quesito_rosa) {
+        Quesito_rosa = quesito_rosa;
+    }
+
+    public boolean isQuesito_azul() {
+        return Quesito_azul;
+    }
+
+    public void setQuesito_azul(boolean quesito_azul) {
+        Quesito_azul = quesito_azul;
+    }
+
+    public boolean isQuesito_morado() {
+        return Quesito_morado;
+    }
+
+    public void setQuesito_morado(boolean quesito_morado) {
+        Quesito_morado = quesito_morado;
+    }
+
+    public boolean isQuesito_naranja() {
+        return Quesito_naranja;
+    }
+
+    public void setQuesito_naranja(boolean quesito_naranja) {
+        Quesito_naranja = quesito_naranja;
+    }
+
+    public boolean isQuesito_verde() {
+        return Quesito_verde;
+    }
+
+    public void setQuesito_verde(boolean quesito_verde) {
+        Quesito_verde = quesito_verde;
+    }
+
+    public boolean isTurno() {
+        return turno;
+    }
+
+    public void setTurno(boolean turno) {
+        this.turno = turno;
+    }
 }
